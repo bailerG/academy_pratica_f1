de0baefe2b1a7964ba4796dc5e26ec65c66d47f3
Guilherme Bailer Motta <guilherme.b.motta@gmail.com>
2023-07-13 18:02:58 -0300

    AP2 Concluida

--- lib/ed1_ap2.dart
+++ lib/ed1_ap2.dart
@@ -1,3 +1,113 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
-void main() {}
+const Color darkBlue = Color.fromARGB(255, 18, 32, 47);
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MyWidget();
+  }
+}
+
+class MyWidget extends StatefulWidget {
+  const MyWidget({super.key});
+
+  @override
+  State<MyWidget> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<MyWidget> {
+  final random = Random();
+
+  var botaoCorreto = 0;
+  var clicks = 0;
+  var acertou = false;
+  var errou = false;
+
+  @override
+  void initState() {
+    botaoCorreto = random.nextInt(3);
+    print(botaoCorreto);
+    super.initState();
+  }
+
+  void botaoPressionado(int botao) {
+    setState(() {
+      if (botao == botaoCorreto) {
+        acertou = true;
+      } else {
+        clicks++;
+      }
+      if (clicks >= 2 && !acertou) {
+        errou = true;
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (acertou) {
+      return MaterialApp(
+        theme: ThemeData.dark()
+            .copyWith(scaffoldBackgroundColor: Colors.green.shade400),
+        debugShowCheckedModeBanner: false,
+        home: const Scaffold(
+          body: Center(
+            child: Text('Você ganhou!'),
+          ),
+        ),
+      );
+    }
+    if (errou) {
+      return MaterialApp(
+        theme: ThemeData.dark()
+            .copyWith(scaffoldBackgroundColor: Colors.red.shade400),
+        debugShowCheckedModeBanner: false,
+        home: const Scaffold(
+          body: Center(
+            child: Text('Você perdeu!'),
+          ),
+        ),
+      );
+    }
+
+    return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: darkBlue,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                  onPressed: (() {
+                    botaoPressionado(0);
+                  }),
+                  child: const Text('A')),
+              ElevatedButton(
+                  onPressed: (() {
+                    botaoPressionado(1);
+                  }),
+                  child: const Text('B')),
+              ElevatedButton(
+                  onPressed: (() {
+                    botaoPressionado(2);
+                  }),
+                  child: const Text('C')),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
